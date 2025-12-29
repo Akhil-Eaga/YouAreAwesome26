064e64675f0c6e782431039306d5cb14f19a3c26
Akhil Eaga <akhil.eaga@bhp.com>
2025-12-29 10:37:48 +0800

    Added a button that uses liquid glass when available.

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var message = "I am a programmer !"
     var body: some View {
         VStack {
             Image(systemName: "swift")
@@ -16,9 +17,31 @@ struct ContentView: View {
                 .frame(width: 250)
                 .foregroundStyle(.green.gradient)
                 .padding()
-            Text("I am a developer!")
+            Text(message)
                 .font(.largeTitle)
                 .fontWeight(.medium)
+            if #available(iOS 26.0, *) {
+                Button {
+                    message = "Awesome !"
+                } label: {
+                    Text("Click Me")
+                        .padding(.vertical, 10)
+                        .padding(.horizontal, 20)
+                }
+                .buttonStyle(.glassProminent)
+            } else {
+                // Fallback on older versions
+                Button {
+                    message = "Awesome !"
+                } label: {
+                    Text("Click Me")
+                        .padding(.vertical, 10)
+                        .padding(.horizontal, 20)
+                }
+                .buttonStyle(.borderedProminent)
+            }
+            
+
         }
         .padding()
     }
